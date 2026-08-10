902db22bd6b6cb0972f78da0b0af7506966b38a2
harry2park-afk <harry2park@gmail.com>
2026-08-11 09:44:34 +1000

    Restore stable room layout while diagnosing client crash

--- src/app/rooms/[id]/layout.tsx
+++ src/app/rooms/[id]/layout.tsx
@@ -1,13 +1,5 @@
 import type { ReactNode } from "react";
-import TranslationBar from "./TranslationBar";
-import CompactChatControls from "./CompactChatControls";
 
 export default function RoomLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <TranslationBar />
-      <CompactChatControls />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
