39be72e7903957c1a6ffc238eafe575f540f348f
harry2park-afk <harry2park@gmail.com>
2026-08-11 09:31:04 +1000

    Wrap rooms with history and work board panels

--- src/app/rooms/[id]/layout.tsx
+++ src/app/rooms/[id]/layout.tsx
@@ -1,13 +1,14 @@
 import type { ReactNode } from "react";
 import TranslationBar from "./TranslationBar";
 import CompactChatControls from "./CompactChatControls";
+import WorkspaceShell from "./WorkspaceShell";
 
 export default function RoomLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <WorkspaceShell>
       <TranslationBar />
       <CompactChatControls />
       {children}
-    </>
+    </WorkspaceShell>
   );
 }
