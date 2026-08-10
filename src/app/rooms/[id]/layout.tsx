ac381f119370d2a77ee166933f83f7ca1e0acf9b
harry2park-afk <harry2park@gmail.com>
2026-08-11 09:22:21 +1000

    Place compact chat controls above room chat

--- src/app/rooms/[id]/layout.tsx
+++ src/app/rooms/[id]/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import TranslationBar from "./TranslationBar";
+import CompactChatControls from "./CompactChatControls";
 
 export default function RoomLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <TranslationBar />
+      <CompactChatControls />
       {children}
     </>
   );
