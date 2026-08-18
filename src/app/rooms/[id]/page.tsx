09247190e4915e18e42fb2e098dbd0e2fe94fc63
harry2park-afk <harry2park@gmail.com>
2026-08-19 00:58:02 +1000

    Bust cache for green SAVE and clickable actions

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -2,7 +2,7 @@ import Script from "next/script";
 import RoomV3 from "./RoomV3";
 import RoomExternalAppClickGuard from "./RoomExternalAppClickGuard";
 
-const ROOM_UI_VERSION = "20260819-0047-bright-actions-always";
+const ROOM_UI_VERSION = "20260819-0058-green-save-actions";
 
 export default function RoomPage() {
   return (
