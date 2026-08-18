19510ca2e6adf13e4ec58982412551ed148ce74a
harry2park-afk <harry2park@gmail.com>
2026-08-19 01:05:23 +1000

    Bust cache for English conversation titles and edit button

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -2,7 +2,7 @@ import Script from "next/script";
 import RoomV3 from "./RoomV3";
 import RoomExternalAppClickGuard from "./RoomExternalAppClickGuard";
 
-const ROOM_UI_VERSION = "20260819-0058-green-save-actions";
+const ROOM_UI_VERSION = "20260819-0102-english-conversation-titles";
 
 export default function RoomPage() {
   return (
