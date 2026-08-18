bbbcae219d6baf5abb6887fe071b027a81abddea
harry2park-afk <harry2park@gmail.com>
2026-08-18 10:06:08 +1000

    Load Command Room English UI normalizer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         <Script src="/rc-language-picker.js" strategy="afterInteractive" />
         <Script src="/rc-copy-question-thread.js" strategy="afterInteractive" />
         <Script src="/rc-question-rules-v2.js" strategy="afterInteractive" />
+        <Script src="/rc-command-room-english.js" strategy="afterInteractive" />
       </body>
     </html>
   );
