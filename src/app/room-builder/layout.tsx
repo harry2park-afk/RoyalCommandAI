f3690edfb2228be89c27c6742aef3375340af2ea
harry2park-afk <harry2park@gmail.com>
2026-08-21 20:04:00 +1000

    Show Command Room return button in Room Builder

--- src/app/room-builder/layout.tsx
+++ src/app/room-builder/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import RoomBuilderScrollUnlock from "./RoomBuilderScrollUnlock";
+import CommandRoomReturnButton from "./CommandRoomReturnButton";
 
 export default function RoomBuilderLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <RoomBuilderScrollUnlock />
+      <CommandRoomReturnButton />
       {children}
     </>
   );
