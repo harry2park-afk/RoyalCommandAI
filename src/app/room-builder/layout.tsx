5aee8bfb2953a554ba13594b2047abcc8ddbee1e
harry2park-afk <harry2park@gmail.com>
2026-08-21 20:43:23 +1000

    Mount Australia Legal English-first locale layer

--- src/app/room-builder/layout.tsx
+++ src/app/room-builder/layout.tsx
@@ -2,12 +2,14 @@ import type { ReactNode } from "react";
 import RoomBuilderScrollUnlock from "./RoomBuilderScrollUnlock";
 import CommandRoomReturnButton from "./CommandRoomReturnButton";
 import RoomBuilderGuide from "./RoomBuilderGuide";
+import AustraliaLegalLocale from "./AustraliaLegalLocale";
 
 export default function RoomBuilderLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <RoomBuilderScrollUnlock />
       <CommandRoomReturnButton />
+      <AustraliaLegalLocale />
       <RoomBuilderGuide />
       {children}
     </>
