944400a5688607ffc19da2ef636cd225af664d70
harry2park-afk <harry2park@gmail.com>
2026-08-22 13:11:23 +1000

    Remove duplicate Room directory from builder

--- src/app/room-builder/layout.tsx
+++ src/app/room-builder/layout.tsx
@@ -2,14 +2,12 @@ import type { ReactNode } from "react";
 import RoomBuilderScrollUnlock from "./RoomBuilderScrollUnlock";
 import CommandRoomReturnButton from "./CommandRoomReturnButton";
 import RoomBuilderGuide from "./RoomBuilderGuide";
-import RoomDirectoryPicker from "./RoomDirectoryPicker";
 
 export default function RoomBuilderLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <RoomBuilderScrollUnlock />
       <CommandRoomReturnButton />
-      <RoomDirectoryPicker />
       <RoomBuilderGuide />
       {children}
     </>
