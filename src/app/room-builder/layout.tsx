11a458d0e3212ce500f6ad3ebe937736b466c1b1
harry2park-afk <harry2park@gmail.com>
2026-08-23 17:37:03 +1000

    Mount Room Guide live input mirror

--- src/app/room-builder/layout.tsx
+++ src/app/room-builder/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import RoomBuilderScrollUnlock from "./RoomBuilderScrollUnlock";
 import CommandRoomReturnButton from "./CommandRoomReturnButton";
 import RoomBuilderAIFormAssistant from "./RoomBuilderAIFormAssistant";
+import RoomGuideLiveInputMirror from "./RoomGuideLiveInputMirror";
 
 export default function RoomBuilderLayout({ children }: { children: ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function RoomBuilderLayout({ children }: { children: ReactNode })
       <RoomBuilderScrollUnlock />
       <CommandRoomReturnButton />
       <RoomBuilderAIFormAssistant />
+      <RoomGuideLiveInputMirror />
       {children}
     </>
   );
