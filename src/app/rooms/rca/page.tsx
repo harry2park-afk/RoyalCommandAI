e2cc3b91840da3e62d3d55200ce1f855c38f887b
harry2park-afk <harry2park@gmail.com>
2026-08-27 15:58:21 +1000

    RCA: render exact RC Room page on static rooms path

---
+++ src/app/rooms/rca/page.tsx
@@ -0,0 +1,10 @@
+import RoomPage from "../[id]/page";
+
+export default function RCARoomPage() {
+  return (
+    <>
+      <script src="/rca-fetch-bridge.js" />
+      <RoomPage />
+    </>
+  );
+}
