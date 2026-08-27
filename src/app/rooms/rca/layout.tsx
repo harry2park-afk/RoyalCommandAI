ea9954f8041bd2aa4764fede45742e62322d8741
harry2park-afk <harry2park@gmail.com>
2026-08-27 16:48:22 +1000

    RCA: use original dynamic Room layout directly

--- src/app/rooms/rca/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import RoomLayout from "../[id]/layout";
-
-export default function RCARoomLayout({ children }: { children: ReactNode }) {
-  return <RoomLayout>{children}</RoomLayout>;
-}
