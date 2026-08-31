c4f263dae5c61c2bcc04fd3899fdb1b733242146
harry2park-afk <harry2park@gmail.com>
2026-08-31 15:29:38 +1000

    fix(room): cache-bust updated room switcher script

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -6,7 +6,7 @@ import RightMenuPasteFix from "./RightMenuPasteFix";
 import SpeakerTtsBridge from "./SpeakerTtsBridge";
 import NativeSynthesisButton from "./NativeSynthesisButton";
 
-const ROOM_UI_VERSION = "20260829-legal-speaker-recovery";
+const ROOM_UI_VERSION = "20260831-room-delete-sync";
 
 export default function RoomPage() {
   return (
