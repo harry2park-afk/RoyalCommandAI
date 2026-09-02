689296042fbea0bab2648ca4951c2b61f61041ff
harry2park-afk <harry2park@gmail.com>
2026-09-02 23:57:20 +1000

    refactor(auth): centralize recovery marker constants

--- src/lib/auth/recovery.ts
+++ src/lib/auth/recovery.ts
@@ -1,4 +1,6 @@
 export const DEFAULT_RECOVERY_PATH = "/update-password";
+export const RECOVERY_COOKIE = "rc_password_recovery";
+export const RECOVERY_COOKIE_MAX_AGE_SECONDS = 10 * 60;
 
 const ALLOWED_RECOVERY_PATHS = new Set([DEFAULT_RECOVERY_PATH]);
 
