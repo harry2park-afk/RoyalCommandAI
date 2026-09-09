520663f92dec089d58ddaa896885aad54e155304
harry2park-afk <harry2park@gmail.com>
2026-09-10 03:51:11 +1000

    [LAUNCH][TEST] Guard commercial provenance coverage

--- src/config/criticalMigrationCoverageContract.test.ts
+++ src/config/criticalMigrationCoverageContract.test.ts
@@ -11,6 +11,7 @@ const REQUIRED_MIGRATIONS = [
   "scope_matter_staff_access",
   "harden_profile_role_authority",
   "country_compliance_evidence_registry",
+  "harden_commercial_review_provenance",
   "payment_operational_safeguards",
   "room_factory_atomic_non_encounter",
   "room_factory_manifest_atomic_only",
